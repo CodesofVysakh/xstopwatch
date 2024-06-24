fabfa5d6c92e2b35e87e043098110a5e15a6251a
CodesOfVysakh <onvysakh309@gmail.com>
2024-06-24 21:52:36 +0530

    stopwatch completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [time, setTime] = useState(0);
+    const [isStarted, setIsStarted] = useState(false);
+
+    useEffect(() => {
+        let intervalId;
+        if (isStarted) {
+            intervalId = setInterval(() => {
+                setTime((prev) => prev + 1000);
+            }, 1000);
+        } else {
+            clearInterval(intervalId);
+        }
+        return () => clearInterval(intervalId);
+    }, [isStarted, time]);
+
+    const handleClick = () => {
+        setIsStarted(!isStarted);
+    };
+    const handleReset = () => {
+        setTime(0);
+    };
+
+    const formatTime = (ms) => {
+        const seconds = Math.floor(ms / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+
+        return `${minutes.toString().padStart(1, "0")}:${remainingSeconds
+            .toString()
+            .padStart(2, "0")}`;
+    };
+
+    return (
+        <div className="App">
+            <h1>Stopwatch</h1>
+            <p>
+                Time : <span>{formatTime(time)}</span>
+            </p>
+            <div className="button-container">
+                <button onClick={() => handleClick()}>
+                    {isStarted ? "Stop" : "Start"}
+                </button>
+                <button onClick={() => handleReset()}>Reset</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
